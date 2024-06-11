51e2cc599934175f3f56f2702d7c046c8f0fa209
ifkah35 <am4666923@gmail.com>
2024-06-11 16:35:24 -0500

    terraform code

---
+++ code3/output.tf
@@ -0,0 +1,12 @@
+output "my-public-ip" {
+  value = aws_lightsail_instance.custom.public_ip_address
+}
+
+output "my-username" {
+  value = aws_lightsail_instance.custom.username
+}
+
+output "key-name" {
+
+  value = aws_lightsail_instance.custom.key_pair_name
+}
